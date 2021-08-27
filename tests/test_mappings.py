69d921d0e2ca9d9b7cbf1c92af9372e3d92cf79f
Bradley P. Allen <bradley.p.allen@gmail.com>
2021-08-26 20:26:09 -0700

    Update test_mappings.py
    Improved roundtrip graph equivalence checking, eliminated unneeded cut-and-paste from test_cli.py

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -60,42 +60,6 @@ TEST_RDF = """
 }
 """
 
-EXPECTED_COMMENT_ROWS = [
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2678',
-  'text': 'First copies printed issued without dustjacket. [M&M]'},
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2677',
-  'text': 'Dustjacket designed by Burroughs.'},
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2681',
-  'text': '3rd printing (1965) issued without dustjacket or decorative border around title page, and with price (“Francs: h18”) printed on back cover.'},
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2679',
-  'text': '1st printing comprised two “issues:”  1st issue: Price (“Francs: 1,500”) printed in lower right corner of back cover. 2nd issue: Price (“New Price NF 18”) stamped over old price in lower right corner of back cover, following the revaluation of the franc in January 1960.'},
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2676',
-  'text': '(The Travellers Companion Series, No. 76)'},
- {'id': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2680',
-  'text': '2nd printing [not to be confused with the “second issue” of the first printing (above)] issued without  dustjacket or decorative border around title page, and with price (“18 francs”) printed on back cover.'}
-]
-
-EXPECTED_BOOK_COMMENT_ROWS = [
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2680'},
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2678'},
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2676'},
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2681'},
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2677'},
- {'subject': 'https://wsburroughs.link/Book/abr40-a2a',
-  'predicate': 'comment',
-  'object': 'https://wsburroughs.link/Comment/n002ffd6182d84355b9967f0665f44e14b2679'}
-]
-
 def test_mapping_roundtrip(tmpdir):
     db_path = tmpdir / "test_mappings.db"
     rdf_path = tmpdir / "test_mappings.jsonld"
@@ -105,4 +69,7 @@ def test_mapping_roundtrip(tmpdir):
     context = "https://schema.org/docs/jsonldcontext.jsonld"
     rdf_to_sqlite(graph_in, context, db)
     graph_out = sqlite_to_rdf(db, context)
-    assert len(graph_in) == len(graph_out)
+    in_vs_out = set(graph_in) - set(graph_out)
+    assert len(in_vs_out) == 0
+    out_vs_in = set(graph_out) - set(graph_in)
+    assert len(out_vs_in) == 0
